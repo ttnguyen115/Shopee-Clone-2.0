d9e02ba53f132e8626d293507b03582918ff3808
Thanh Trung Nguyen <thanhtrung1012000@gmail.com>
2022-11-30 22:52:30 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import useRouteElements from './hooks'
+
+function App() {
+  const routeElements = useRouteElements()
+
+  return <div>{routeElements}</div>
+}
+
+export default App
---
+++ src/hooks/useRouteElements.tsx
@@ -0,0 +1,32 @@
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from 'src/layouts/RegisterLayout'
+import Login from 'src/pages/Login'
+import ProductList from 'src/pages/ProducList'
+import Register from 'src/pages/Register'
+
+export default function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    }
+  ])
+
+  return routeElements
+}
