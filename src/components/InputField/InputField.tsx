505e7045797d84f801f3d740e7c570c4cb847094
Thanh Trung Nguyen <thanhtrung1012000@gmail.com>
2022-12-01 22:56:23 +0700

    feat: add InputField components

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,36 @@
+import type { RegisterOptions, UseFormRegister } from 'react-hook-form'
+
+interface Props {
+  type: React.HTMLInputTypeAttribute
+  errorMessage?: string
+  placeholder?: string
+  className?: string
+  name: string
+  register: UseFormRegister<any>
+  rules?: RegisterOptions
+  autoComplete?: string
+}
+
+export default function InputField({
+  type,
+  errorMessage,
+  placeholder,
+  className,
+  name,
+  register,
+  rules,
+  autoComplete
+}: Props) {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        className='w-full border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        {...register(name, rules)}
+      />
+      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+    </div>
+  )
+}
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import InputField from 'src/components/InputField'
 import { getRules } from 'src/utils'
 
 interface FormData {
@@ -35,35 +36,35 @@ export default function Register() {
           <div className='lg:col-span-2 lg:col-start-4'>
             <form onSubmit={onSubmit} className='rounded bg-white p-10 shadow-sm'>
               <div className='text-2xl'>Đăng ký</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  className='w-full border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Email'
-                  {...register('email', rules.email)}
-                />
-                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors?.email?.message}</div>
-              </div>
-              <div className='mt-2'>
-                <input
-                  type='password'
-                  className='w-full border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Password'
-                  autoComplete='on'
-                  {...register('password', rules.password)}
-                />
-                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors?.password?.message}</div>
-              </div>
-              <div className='mt-2'>
-                <input
-                  type='password'
-                  className='w-full border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-                  placeholder='Confirm Password'
-                  autoComplete='on'
-                  {...register('confirm_password', rules.confirm_password)}
-                />
-                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors?.confirm_password?.message}</div>
-              </div>
+              <InputField
+                name='email'
+                register={register}
+                type='email'
+                placeholder='Email'
+                className='mt-8'
+                errorMessage={errors?.email?.message}
+                rules={rules.email}
+              />
+              <InputField
+                name='password'
+                register={register}
+                type='password'
+                placeholder='Password'
+                className='mt-2'
+                errorMessage={errors?.password?.message}
+                rules={rules.password}
+                autoComplete='on'
+              />
+              <InputField
+                name='confirm_password'
+                register={register}
+                type='password'
+                placeholder='Confirm Password'
+                className='mt-2'
+                errorMessage={errors?.confirm_password?.message}
+                rules={rules.confirm_password}
+                autoComplete='on'
+              />
               <div className='mt-2'>
                 <button className='w-full bg-red-500 py-4 px-2 text-center text-sm uppercase text-white hover:bg-red-600'>
                   Đăng ký
