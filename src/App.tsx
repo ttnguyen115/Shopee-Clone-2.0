4e28d86238d07ff4e9c52bf2af19c870cffc2479
Thanh Trung Nguyen <thanhtrung1012000@gmail.com>
2022-12-02 22:00:38 +0700

    fix: add react-toastify and axios interceptor to handle unexpected error from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import useRouteElements from './hooks'
 
 function App() {
   const routeElements = useRouteElements()
 
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
--- src/services/http.ts
+++ src/services/http.ts
@@ -1,5 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
-
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
+import { HttpStatusCode } from 'src/constants'
 class Http {
   instance: AxiosInstance
 
@@ -11,6 +12,22 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        if (error) return Promise.reject(error)
+      }
+    )
   }
 }
 
