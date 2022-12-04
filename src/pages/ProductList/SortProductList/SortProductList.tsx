25de0f3544914697ab9c298b25a2ff1d3fbaed1e
Thanh Trung Nguyen <thanhtrung1012000@gmail.com>
2022-12-04 20:49:50 +0700

    feat: add sort functions in ProductList page

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,44 +1,139 @@
+import clsx from 'clsx'
+import _ from 'lodash'
+import { createSearchParams, Link, useNavigate } from 'react-router-dom'
+
 import { ReactComponent as ChevronLeftSvg } from 'src/assets/chevron-left.svg'
 import { ReactComponent as ChevronRightSvg } from 'src/assets/chevron-right.svg'
 
-export default function SortProductList() {
+import type { ProductListConfig } from 'src/types/product'
+import { QueryConfig } from '../ProductList'
+
+import { AppRoutes } from 'src/constants'
+import { order as orderConstants, sortBy } from 'src/constants/product'
+
+interface Props {
+  queryConfig: QueryConfig
+  pageSize: number
+}
+
+export default function SortProductList({ queryConfig, pageSize }: Props) {
+  const { sort_by = sortBy.createdAt, order, page } = queryConfig
+  const pageCurrent = Number(page)
+  const navigate = useNavigate()
+  const classNameFromSortBy = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>): string => {
+    return sort_by === sortByValue
+      ? 'bg-orange text-white hover:bg-orange/80'
+      : 'bg-white text-black hover:bg-slate-100'
+  }
+
+  const handleSort = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    navigate({
+      pathname: AppRoutes.APP_DEFAULT,
+      search: createSearchParams(
+        _.omit(
+          {
+            ...queryConfig,
+            sort_by: sortByValue
+          },
+          ['order']
+        )
+      ).toString()
+    })
+  }
+
+  const handlePriceOrder = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const order = event.target.value as Exclude<ProductListConfig['order'], undefined>
+    navigate({
+      pathname: AppRoutes.APP_DEFAULT,
+      search: createSearchParams({
+        ...queryConfig,
+        sort_by: sortBy.price,
+        order
+      }).toString()
+    })
+  }
+
   return (
     <div className='bg-gray-300/40 py-4 px-3'>
       <div className='flex flex-wrap items-center justify-between gap-2'>
         <div className='flex flex-wrap items-center gap-2'>
           <div>Sắp xếp theo</div>
-          <button className='h-8 bg-orange px-4 text-center text-sm capitalize text-white hover:bg-orange/80'>
+          <button
+            onClick={() => handleSort(sortBy.view)}
+            className={clsx('h-8 px-4 text-center text-sm capitalize', classNameFromSortBy(sortBy.view))}
+          >
             Phổ biến
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            onClick={() => handleSort(sortBy.createdAt)}
+            className={clsx('h-8 px-4 text-center text-sm capitalize', classNameFromSortBy(sortBy.createdAt))}
+          >
             Mới nhất
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            onClick={() => handleSort(sortBy.sold)}
+            className={clsx('h-8 px-4 text-center text-sm capitalize', classNameFromSortBy(sortBy.sold))}
+          >
             Bán chạy
           </button>
           <select
-            className='h-8 bg-white px-4 text-center text-sm capitalize text-black outline-none hover:bg-slate-100'
-            defaultValue=''
+            className={clsx('h-8 px-4 text-center text-sm capitalize outline-none', classNameFromSortBy(sortBy.price))}
+            value={order || ''}
+            onChange={handlePriceOrder}
           >
-            <option value='' disabled>
+            <option value='' disabled className='bg-white text-black'>
               Giá
             </option>
-            <option value='price:asc'>Giá: Thấp đến cao</option>
-            <option value='price:desc'>Giá: Cao đến thấp</option>
+            <option value={orderConstants.asc} className='bg-white text-black'>
+              Giá: Thấp đến cao
+            </option>
+            <option value={orderConstants.desc} className='bg-white text-black'>
+              Giá: Cao đến thấp
+            </option>
           </select>
         </div>
         <div className='flex items-center'>
           <div>
-            <span className='text-orange'>1</span>
-            <span>/2</span>
+            <span className='text-orange'>{pageCurrent}</span>
+            <span>/{pageSize}</span>
           </div>
           <div className='ml-2 flex'>
-            <button className='h-8 cursor-not-allowed rounded-tl-sm rounded-bl-sm bg-white/60 px-3 hover:bg-slate-100'>
-              <ChevronLeftSvg />
-            </button>
-            <button className='h-8 rounded-tr-sm rounded-br-sm bg-white px-3 hover:bg-slate-100'>
-              <ChevronRightSvg />
-            </button>
+            {pageCurrent === 1 ? (
+              <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'>
+                <ChevronLeftSvg />
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: AppRoutes.APP_DEFAULT,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    page: (pageCurrent - 1).toString()
+                  }).toString()
+                }}
+                className='flex h-8 w-9 items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'
+              >
+                <ChevronLeftSvg />
+              </Link>
+            )}
+            {pageCurrent === pageSize ? (
+              <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'>
+                <ChevronRightSvg />
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: AppRoutes.APP_DEFAULT,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    page: (pageCurrent + 1).toString()
+                  }).toString()
+                }}
+                className='flex h-8 w-9 items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'
+              >
+                <ChevronRightSvg />
+              </Link>
+            )}
           </div>
         </div>
       </div>
